32581c06fbfb737bb83488f32419915909ed672b
autotune <b@contrasting.org>
2015-07-09 05:39:58 +0000

    Removed sample data from serverspec

--- cookbooks/tf2server/spec/rhce-stage-tf2server/tf2server_spec.rb
+++ cookbooks/tf2server/spec/rhce-stage-tf2server/tf2server_spec.rb
@@ -4,8 +4,3 @@ describe service('tf2-server'), :if => os[:family] == 'redhat' do
   it { should be_running.under('upstart') }
 end
 
-describe service('apache2'), :if => os[:family] == 'ubuntu' do
-  it { should be_enabled }
-  it { should be_running }
-end
-
