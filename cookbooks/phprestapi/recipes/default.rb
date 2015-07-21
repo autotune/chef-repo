804cfd59397fdc4a4d461e7ecdd0e1e73b7a9654
autotune <b@contrasting.org>
2015-07-20 23:39:27 -0600

    Added remove mysql-libs without deps

--- cookbooks/phprestapi/recipes/default.rb
+++ cookbooks/phprestapi/recipes/default.rb
@@ -20,18 +20,24 @@ when "rhel"
 # install required packages 
 # exact versions in /var/log/yum.log
   yum_package 'system_packages' do
-    package_name ['wget', 'git' ]
-    version ['1.12-5.el6_6.1', '1.7.1-3.el6_4.1']
+    package_name ['wget', 'git', 'yum-plugin-replace']
+    version ['1.12-5.el6_6.1', '1.7.1-3.el6_4.1', '0.2.7-1.ius.centos6']
     flush_cache[ :before ]
     action :install
 end
 
-#   yum_package 'database_packages' do
-#   package_name ['mysql55-server' ]
-#     version ['5.3.3-46.el6_6']
-#     flush_cache[ :before ]
-#     action :install
-# end
+  # replace mysql-libs with mysql55-libs
+  execute "remove mysql-libs without removing deps" do
+  command "rpm -e mysql-libs --nodeps"
+  action :run
+  end 
+
+  yum_package 'database_packages' do
+  package_name ['mysql55-server' ]
+    version ['5.5.44-2.ius.centos6']
+    flush_cache[ :before ]
+    action :install
+  end
 
   yum_package 'web_packages' do
   package_name ['php' ] 
